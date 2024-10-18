990c0ec7b6ebb40e92875c7d9a9366ae9eec779e
Dominik Chrastecky <dominik@chrastecky.cz>
2024-10-18 20:19:50 +0200

    Return URLs to comments/combined

--- dto/SuccessResponse.go
+++ dto/SuccessResponse.go
@@ -1,13 +1,17 @@
 package dto
 
 type SuccessResponse struct {
-	Message string `json:"message"`
-	Url     string `json:"url"`
+	Message  string `json:"message"`
+	Posts    string `json:"posts"`
+	Comments string `json:"comments"`
+	Both     string `json:"both"`
 }
 
 func NewSuccessResponse(url string) *SuccessResponse {
 	return &SuccessResponse{
-		Message: "Success! You can find your feed at " + url,
-		Url:     url,
+		Message:  "Success! You can find your feed at " + url,
+		Posts:    url,
+		Comments: url + "?include=comments",
+		Both:     url + "?include=posts,comments",
 	}
 }
